2e58952516bc9b6261de555f103a9e903b8b1370
An Lou <louan1734@gmail.com>
2025-09-29 16:13:21 -0500

    control header

--- data/enka.js
+++ data/enka.js
@@ -1,7 +1,12 @@
 const UID = 621003558;
 
 export async function getUserData() {
-  fetch(`https://enka.network/api/uid/${UID}/?info`, {})
+  fetch(`https://enka.network/api/uid/${UID}/?info`, {
+    headers: {
+      "Access-Control-Allow-Origin":
+        "https://woopxwoop.github.io/Portfolio-Website/",
+    },
+  })
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
