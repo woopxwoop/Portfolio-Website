d129fcb8b24841751a53af550c51afe0b55d1461
An Lou <louan1734@gmail.com>
2025-10-18 18:39:46 -0500

    made photo go above text on small screens

--- data/pictures.js
+++ data/pictures.js
@@ -1,9 +1,11 @@
 export let pictures = [
+  /*
   { src: "images/cropped_chuzz.jpg", alt: "picture of me, Andrew Lou" },
   {
     src: "images/gaysicle.jpg",
     alt: "picture of me, Andrew Lou, with a rainbow popsicle",
   },
+  */
   {
     src: "images/haircut.jpg",
     alt: "picture of me, Andrew Lou",
