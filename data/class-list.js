6ff29b268817e9b19614d930535deb8ad8d820f2
An Lou <louan1734@gmail.com>
2025-11-18 19:25:55 -0600

    update projects

--- data/class-list.js
+++ data/class-list.js
@@ -148,11 +148,60 @@ export let classList = [
   },
   {
     listing: "CS540",
-    description: "Intro to AI",
+    description: "Intro to AI [Python]",
+    projects: [
+      {
+        title: "English or Spanish",
+        description:
+          "Wrote a program which applied naive bayes to determine the likelihood of text input being English or Spanish",
+      },
+      {
+        title: "Celebrity PCA",
+        description:
+          "Wrote a program to compress and reconstruct images from the celebA dataset using Principle Component Analysis",
+      },
+      {
+        title: "n-gram",
+        description:
+          "Wrote a program to predict and generate sequences of letters using an n-gram model",
+      },
+      {
+        title: "Countries Hierarchical Clustering",
+        description:
+          "Create a dendrogram showing the relatedness of different countries with respect to gdp, income, life expectancy, etc... under complete-linkage",
+      },
+      {
+        title: "Lake Mendota Warming",
+        description:
+          "Verify the effects of global warming by creating a linear regression model using backpropagation to show how Lake Mendota has less frozen days over time",
+      },
+      {
+        title: "Classifying Clothes",
+        description:
+          "Used Pytorch to train a multi-layer perceptron on the FashionMNIST dataset to classify clothing with 90% accuracy",
+      },
+      {
+        title: "Modern Shakespeare",
+        description:
+          "Trained nanoGPT on Shakespeare to generate some not so poetic text",
+      },
+      {
+        title: "8(7)-Puzzle Game",
+        description:
+          "Implement the A* algorithm to solve a modified version of the 8-Puzzle game with only 7 tiles. ",
+      },
+    ],
   },
   {
     listing: "CS571",
-    description: "UI/UX/Frontend",
+    description: "UI/UX/Frontend [JS/React]",
+    projects: [
+      {
+        title: "Weather App",
+        description:
+          "CURRENTLY WRITING a React Application to fetch user's desired weather data and display it using a Vercel backend and Redis caching",
+      },
+    ],
   },
   {
     listing: "CS577",
